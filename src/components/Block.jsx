22055a4175bc1b7beb188462eee7d9d4a120bac2
Monjur Khuda <kiatbrooklyn@gmail.com>
2023-10-21 00:02:02 -0400

    wip

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -5,8 +5,8 @@ import Locale from "./Locale";
 function Block({ type, localeType, localeName, lat, long, country }) {
   return (
     <div className={`block_container ${type} ${country}`}>
-      <Locale localeType={localeType} localeName={localeName} />
       {lat},{long}
+      <Locale localeType={localeType} localeName={localeName} />
     </div>
   );
 }
