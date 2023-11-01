0b5ea08ad138568687beee7be95bd893c7cbd036
Monjur Khuda <kiatbrooklyn@gmail.com>
2023-10-31 22:49:44 -0400

    int borders wip

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -11,9 +11,47 @@ function Block({
   country,
   region,
   showCoordinates,
+  mapObj,
 }) {
+  let borderClassName = "";
+
+  console.log(
+    mapObj[`${lat}${long}`]?.country,
+    mapObj[`${lat}${long - 1}`]?.country,
+    lat,
+    long
+  );
+
+  console.log(
+    mapObj[`${lat}${long}`]?.country !== mapObj[`${lat}${long - 1}`]?.country
+  );
+
+  if (
+    mapObj[`${lat}${long}`]?.country !== mapObj[`${lat}${long - 1}`]?.country
+  ) {
+    borderClassName += " leftBorder";
+  }
+
+  if (
+    mapObj[`${lat}${long}`]?.country !== mapObj[`${lat}${long + 1}`]?.country
+  ) {
+    borderClassName += " rightBorder";
+  }
+
+  if (
+    mapObj[`${lat}${long}`]?.country !== mapObj[`${lat + 1}${long}`]?.country
+  ) {
+    borderClassName += " bottomBorder";
+  }
+
+  if (
+    mapObj[`${lat}${long}`]?.country !== mapObj[`${lat - 1}${long}`]?.country
+  ) {
+    borderClassName += " topBorder";
+  }
+
   return (
-    <div className={`block_container ${type} ${country}`}>
+    <div className={`block_container ${type} ${country} ${borderClassName}`}>
       {showCoordinates && (
         <>
           {lat}, {long}
